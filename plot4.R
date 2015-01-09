402ef20916360c54c2adcda6075b18a0b50d31cf
sumatnafisa <sn2834@att.com>
2015-01-08 22:33:11 -0500

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,4 +1,4 @@
-data_full <- read.csv(".household_power_consumption.txt", header=T, sep=';', na.strings="?",  
+data_full <- read.csv("./household_power_consumption.txt", header=T, sep=';', na.strings="?",  
                      nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"') 
 data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y") 
 
